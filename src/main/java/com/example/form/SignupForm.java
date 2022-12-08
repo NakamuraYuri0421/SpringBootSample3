d5cd0cb98cd129654781c293b4109bce88ed9af0
NakamuraYuri0421 <nkmr44yr@gmail.com>
2022-12-09 00:28:01 +0900

    6.3.1章　修正

--- src/main/java/com/example/form/SignupForm.java
+++ src/main/java/com/example/form/SignupForm.java
@@ -16,21 +16,27 @@ import lombok.Data;
 
 @Data
 public class SignupForm {
+	
 	@NotBlank
 	@Email
 	private String userId;
+	
 	@NotBlank
 	@Length(min=4,max=100)
 	@Pattern(regexp="^[a-zA-Z0-9]+$")
 	private String password;
+	
 	@NotBlank
 	private String userName;
+	
 	@DateTimeFormat(pattern="yyyy/MM/dd")
 	@NotNull
 	private Date birthday;
+	
 	@Min(20)
 	@Max(100)
 	private Integer age;
+	
 	@NotNull
 	private Integer gender;
 
