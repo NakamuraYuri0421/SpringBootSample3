833b1d686f8d0a8413044a912abac5515838bcf8
NakamuraYuri0421 <nkmr44yr@gmail.com>
2022-12-09 00:20:23 +0900

    6.2.1章　修正

--- src/main/java/com/example/form/SignupForm.java
+++ src/main/java/com/example/form/SignupForm.java
@@ -11,6 +11,7 @@ public class SignupForm {
 	private String userId;
 	private String password;
 	private String userName;
+
 	@DateTimeFormat(pattern="yyyy/MM/dd")
 	private Date birthday;
 	private Integer age;
