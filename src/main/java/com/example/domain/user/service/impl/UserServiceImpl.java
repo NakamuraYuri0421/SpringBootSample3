47558024c906188a2d0b35908bfc287af28b7dc3
NakamuraYuri0421 <nkmr44yr@gmail.com>
2022-12-17 09:59:28 +0900

    11.2.4章　3周目

--- src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.domain.user.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,12 +17,19 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserMapper mapper;
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	/**ユーザー登録*/
 	@Override
 	public void signup(MUser user) {
 		user.setDepartmentId(1);//部署
 		user.setRole("ROLE_GENERAL");//ロール
 		mapper.insertOne(user);
+		
+		//パスワード暗号化
+		String rawPassword = user.getPassword();
+		user.setPassword(encoder.encode(rawPassword));
 	}
 	
 	/**ユーザー取得*/
@@ -41,6 +49,10 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public void updateUserOne(String userId,String password,String userName) {
 		mapper.updateOne(userId,password,userName);
+		
+		//パスワード暗号化
+		String encryptPassword=encoder.encode(password);
+		mapper.updateOne(userId, encryptPassword, userName);
 		
 	}
 	
